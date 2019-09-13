9bc6e9342c4c0a753acff4f57af610645a129a1f
Altug Sahin <altug@aecoffice.com>
2019-09-13 18:44:00 -0400

    added column widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,34 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answer() {
+    print("clicked");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("title"),
+          title: Text("Quiz App"),
         ),
-        body: Text("hello there"),
+        body: Column(
+          children: <Widget>[
+            Text("question"),
+            RaisedButton(
+              child: Text("answer 1"),
+              onPressed: answer,
+            ),
+            RaisedButton(
+              child: Text("answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
